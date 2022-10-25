55d7eee00bca5b6d1e2b9a42eaa1e7134628e93a
Felipe Sander <felipe.panisson@hotmail.com>
2022-10-25 16:28:05 -0300

    initial styles for header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,21 @@ import { AppProps } from "next/app"
 
 import { globalStyles } from "../styles/global"
 
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from "../styles/pages/app";
+
 globalStyles();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="Logo" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
 
 export default MyApp

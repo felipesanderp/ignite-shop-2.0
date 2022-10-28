e1cf19f29c7080859ad522a02086adadb3be2d2d
Felipe Sander <felipe.panisson@hotmail.com>
2022-10-28 16:24:21 -0300

    get product details from stripe

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,23 +1,62 @@
+import { GetStaticProps } from "next";
+import Image from "next/image";
 import { useRouter } from "next/router"
+import Stripe from "stripe";
+import { stripe } from "../../lib/stripe";
+
 import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product";
 
-export default function Product() {
-  const { query } = useRouter();
+interface ProductProps {
+  product: {
+    id: string;
+    name: string;
+    imageURL: string;
+    price: string;
+    description: string;
+  }
+}
 
+export default function Product({ product }: ProductProps) {
   return (
     <ProductContainer>
       <ImageContainer>
-
+        <Image src={product.imageURL} width={520} height={480} alt="" />
       </ImageContainer>
 
       <ProductDetails>
-        <h1>Camiseta X</h1>
-        <span>R$ 79,90</span>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
 
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum laboriosam ducimus, possimus provident odit earum voluptates autem vero velit! Nisi beatae culpa sequi autem perferendis nesciunt nam eligendi esse et.</p>
+        <p>{product.description}</p>
 
         <button>Comprar agora</button>
       </ProductDetails>
     </ProductContainer>
   )
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+  const productId = params.id;
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price']
+  })
+
+  const price = product.default_price as Stripe.Price;
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageURL: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(price.unit_amount / 100),
+        description: product.description,
+      }
+    },
+    revalidate: 60 * 60 * 1, //1 hour
+  }
 }

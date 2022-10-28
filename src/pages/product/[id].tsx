bfed04ea11c0dc7fea4893f11b9951acc093024c
Felipe Sander <felipe.panisson@hotmail.com>
2022-10-28 16:01:58 -0300

    create layout for product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,23 @@
 import { useRouter } from "next/router"
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product";
 
 export default function Product() {
   const { query } = useRouter();
 
   return (
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum laboriosam ducimus, possimus provident odit earum voluptates autem vero velit! Nisi beatae culpa sequi autem perferendis nesciunt nam eligendi esse et.</p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
